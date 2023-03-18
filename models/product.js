d6afdf6bdd94b187a941030305c618c3c8dc37dc
Dan Stronge <dnlstronge@gmail.com>
2023-03-18 10:15:05 +0000

    [x] - add values to schema

--- models/product.js
+++ models/product.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-
+    name: {
+        type: String , 
+        required: [true, "product name must be provided"] 
+    },
+    price: {
+        type: Number, 
+        required: [true, "product must have a price"]
+    }
 })
