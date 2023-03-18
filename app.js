f4167910e939667b9a66eb0b973ac61326a09806
Dan Stronge <dnlstronge@gmail.com>
2023-03-18 07:52:33 +0000

    [x] - init proj [x] - add imports to app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('04 Store API')
+require("dotenv").config()
+// async error
+
+
+const express = require("express");
+const app = express();
+
+// error handlers: 
+const errorHandler = require("./middleware/error-handler");
+const notFound = reqquire("./middleware/not-found.js");
+
