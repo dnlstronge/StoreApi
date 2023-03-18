76397a5cf0827b0bf1d98c784dbc18271bb2b4b4
Dan Stronge <dnlstronge@gmail.com>
2023-03-18 08:02:09 +0000

    [x] - complete basic set up with route and MW

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-require("dotenv").config()
+require("dotenv").config();
 // async error
-
 
 const express = require("express");
 const app = express();
 
-// error handlers: 
+// error handlers:
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFoundMiddleware = require("./middleware/not-found.js");
 
-// json middleware: 
+// json middleware:
 app.use(express.json());
 
-// test route: 
+// test route:
 
 app.get("/", (req, res) => {
-    res.send("<h1>Store API</h1>")
-})
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>Products Route</a>");
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
