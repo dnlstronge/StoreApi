5d8be338a781164ed1b2992175ce17b2ec1f73e7
Dan Stronge <dnlstronge@gmail.com>
2023-03-19 08:45:00 +0000

    [x] - also return number of hits

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({company: "ikea"})
-  res.status(200).json({products});
+  res.status(200).json({products, nbHits: products.length});
 };
 
 const getAllProducts = async (req, res) => {
