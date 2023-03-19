679941b5efdb8fa855bec2b2c71134d920c9a725
Dan Stronge <dnlstronge@gmail.com>
2023-03-19 10:12:19 +0000

    [x] - add name query

--- controllers/products.js
+++ controllers/products.js
@@ -9,15 +9,18 @@ const getAllProductsStatic = async (req, res) => {
 // get all products by ===> 
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query
+  const { featured, company, name } = req.query
   const queryObject = {}
   if(featured) {
     queryObject.featured = featured === "true" ? true : false
   }
-  if(company) [
+  if(company) {
     queryObject.company = company
-  ]
-  console.log(queryObject)
+  }
+  if(name) {
+    queryObject.name = name
+  }
+  // console.log(queryObject)
   const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length });
 };
