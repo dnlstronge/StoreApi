de1174928c8ba69b6d99fed1e7b86dd19481ea30
Dan Stronge <dnlstronge@gmail.com>
2023-03-20 08:20:31 +0000

    [x] - remove name search and regex

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,8 @@ const Product = require("../models/product")
 
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "a"
-  const products = await Product.find({
-    name: {
-    $regex: search, $options: "i"
-  }
-  })
+ 
+ const products = await Product.find({})
   res.status(200).json({products, nbHits: products.length});
 };
 
