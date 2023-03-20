a57334d0f6e98141446fe90ca7f9d692ad64045d
Dan Stronge <dnlstronge@gmail.com>
2023-03-20 08:49:04 +0000

    [x] - format sort

--- controllers/products.js
+++ controllers/products.js
@@ -22,9 +22,10 @@ const getAllProducts = async (req, res) => {
     queryObject.name = {$regex: name, $options: "i"}
   }
   // console.log(queryObject)
-  let reesult = Product.find(queryObject)
+  let result = Product.find(queryObject)
   if(sort) {
-    products = products.sort()
+    const sortList = sort.split(",").join(" ")
+    result = sort(sortList)
   }
   const products = await result
   res.status(200).json({ products, nbHits: products.length });
