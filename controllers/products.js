3e1f2d586b3885a6134486a8624f33497ea10dd2
Dan Stronge <dnlstronge@gmail.com>
2023-03-21 07:33:00 +0000

    [x] - demo skip

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,8 @@ const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({})
     .sort("name")
     .select("name price")
-    .limit(10);
+    .limit(10)
+    .skip(1)
   res.status(200).json({ products, nbHits: products.length });
 };
 
