be714912175f41c92e05e8d4c4727ee8d7e16e25
Dan Stronge <dnlstronge@gmail.com>
2023-03-21 07:30:43 +0000

    [x] - add sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,43 +1,44 @@
-const Product = require("../models/product")
-
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
- 
- const products = await Product.find({}).select("name price").limit(4)
-  res.status(200).json({products, nbHits: products.length});
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-// get all products by ===> 
+// get all products by ===>
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query
-  const queryObject = {}
-  if(featured) {
-    queryObject.featured = featured === "true" ? true : false
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
   }
-  if(company) {
-    queryObject.company = company
+  if (company) {
+    queryObject.company = company;
   }
-  if(name) {
-    queryObject.name = {$regex: name, $options: "i"}
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
   // console.log(queryObject)
-  let result = Product.find(queryObject)
+  let result = Product.find(queryObject);
 
   //sort
-  if(sort) {
-    const sortList = sort.split(",").join(" ")
-    result = result.sort(sortList)
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
   } else {
-    result = result.sort(createAt)
+    result = result.sort(createAt);
   }
 
-if(fields) {
-  const fieldList = fields.split(",").join(" ")
-  result = result.select(fieldList)
-}
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
   //select
-  const products = await result
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
