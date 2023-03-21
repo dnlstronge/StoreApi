ede2c79659004775878ceb77f2f20cf74ff05a1c
Dan Stronge <dnlstronge@gmail.com>
2023-03-21 07:46:22 +0000

    [x] - add comments for logic

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic = async (req, res) => {
     .sort("name")
     .select("name price")
     .limit(10)
-    .skip(1)
+    .skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -40,7 +40,17 @@ const getAllProducts = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit)
+// so 23 items => limit of 7, = pages => 7, 7, 7, 2
+// this works because page 1 - 1 = 0, and 7 (limit) * 0 == 0 ===> none are skipped (first 7 displayed)
+// now if page 2, 2 (page) - 1 = 1, 1 * 7 (limit) = 7 ===> 7 are skipped
+// and page 3, 3 (page -1) - 2, 2 * 7 (limit) = 14 ===> 14 are skipped and so on......
+
+
+
+
   //select
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
